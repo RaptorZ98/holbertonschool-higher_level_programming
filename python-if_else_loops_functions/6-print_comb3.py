0730c941dfb2d4be46592177750605eca22724fe
RaptorZ98 <federicoheuercor@hotmail.com>
2023-05-30 07:24:27 -0700

    not print 00

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 9):
     for n in range(i, 10):
-        print("{}{}, ".format(i, n), end="")
+        if (n > 0):
+            print("{}{}, ".format(i, n), end="")
 print("89")
