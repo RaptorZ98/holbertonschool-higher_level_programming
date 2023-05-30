a5d0484473e47a3f00b7b85ca2784b5b4d7c106e
RaptorZ98 <federicoheuercor@hotmail.com>
2023-05-30 07:27:20 -0700

    corrected i into i + 1

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 9):
-    for n in range(i, 10):
-        if (n > 0):
+    for n in range(i + 1, 10):
+        if i < 8:
             print("{}{}, ".format(i, n), end="")
 print("89")
