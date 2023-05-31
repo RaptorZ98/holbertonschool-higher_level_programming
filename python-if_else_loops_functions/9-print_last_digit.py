f4b1a1cd0f478cb4fa0fab4e7793bc55e2d90904
RaptorZ98 <federicoheuercor@hotmail.com>
2023-05-30 20:43:44 -0700

    changed if number == 0

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -2,8 +2,10 @@
 def print_last_digit(number):
     if number < 0:
         number %= -10
-        print("{}".format(-number), end="")
-        return -number
+        print("{}".format(number * -1), end="")
+        return number * -1
+    elif number == 0:
+        return 0
     number % 10
     print("{}".format(number), end="")
     return number
