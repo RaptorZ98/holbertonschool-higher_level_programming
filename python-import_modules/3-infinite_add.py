78138601251e0d239a2bb6c23def0811b61ee49d
RaptorZ98 <federicoheuercor@hotmail.com>
2023-05-31 19:53:07 -0700

    changed n for i

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -2,7 +2,9 @@
 from sys import argv
 if __name__ == "__main__":
     n = 0
+    i = 0
     for nums in argv:
-        if n != 0:
+        if i != 0:
             n += int(nums)
+        i = 1
     print("{}".format(n))
