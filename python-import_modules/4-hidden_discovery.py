5dac5182e7768ef7473468b1be7554568dffa64c
RaptorZ98 <federicoheuercor@hotmail.com>
2023-06-04 13:34:46 -0700

    added function dir in 4

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import hidden_4.pyc
 if __name__ == "__main__":
-    for names in hidden_4:
+    for names in dir(hidden_4):
         if names[0] != "_":
             print("{}".format(names))
