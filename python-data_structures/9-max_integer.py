943e82a6f7f71ec3c50af84976fbf8c48da5c553
RaptorZ98 <federicoheuercor@hotmail.com>
2023-06-11 11:40:49 -0700

    changed i for my_list[i]

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -3,8 +3,8 @@ def max_integer(my_list=[]):
     if my_list:
         x = my_list[0]
         for i in range(len(my_list)):
-            if i > x:
-                x = i
+            if my_list[i] > x:
+                x = my_list[i]
         return x
     else:
         return None
