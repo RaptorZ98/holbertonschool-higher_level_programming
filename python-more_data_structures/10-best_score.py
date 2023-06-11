5a8a2bbccda3324ae4c2a0176bdc9b53e1e0c88b
RaptorZ98 <federicoheuercor@hotmail.com>
2023-06-11 15:07:22 -0700

    changed the accesing of values

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,9 +2,9 @@
 def best_score(a_dictionary):
     if a_dictionary:
         res = 0
-        for num in a_dictionary:
-            if res < a_dictionary[num]:
-                res = a_dictionary[num]
+        for num in a_dictionary.values():
+            if res < num:
+                res = num
         return res
     else:
         return None
