f8680a9accd6b20301f0d9a32a5154b86dc6f32f
RaptorZ98 <federicoheuercor@hotmail.com>
2023-06-11 16:54:17 -0700

    changed the line jump

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,8 +3,9 @@ def safe_print_list(my_list=[], x=0):
     res = 0
     for i in range(x):
         try:
-            print("{}".format(my_list[i]))
+            print("{}".format(my_list[i]), end="")
             res += 1
         except IndexError:
             break
+    print("")
     return res
