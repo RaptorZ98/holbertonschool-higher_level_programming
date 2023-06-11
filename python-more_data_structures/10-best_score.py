ac1d70a235db4d5e58995f7d7df462cc7dbb6b7d
RaptorZ98 <federicoheuercor@hotmail.com>
2023-06-11 15:02:06 -0700

    added function 10

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary:
+        res = 0
+        for num in a_dictionary:
+            if res < a_dictionary[num]:
+                res = a_dictionary[num]
+    return res
