6037b8c745a89b935739743438cd393e56dbdeb0
RaptorZ98 <federicoheuercor@hotmail.com>
2023-06-11 11:10:18 -0700

    changed 1 for 0

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     if matrix:
-        t = 1
+        t = 0
         for x in matrix:
             for num in x:
                 if t == 1:
                     print(" ", end="")
                 print("{:d}".format(num), end="")
-                t = 0
-            t = 1
+                t = 1
+            t = 0
             print("")
     else:
         print("")
