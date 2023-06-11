0e5ecaf7762b5f7bd8f8e5dbcd8c3b442d27c7e1
RaptorZ98 <federicoheuercor@hotmail.com>
2023-06-11 14:58:20 -0700

    added a new dictionary

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
+    new_d = dict()
     for key in a_dictionary:
-        a_dictionary.update({key: a_dictionary[key] * 2})
-    return a_dictionary
+        new_d.update({key: a_dictionary[key] * 2})
+    return new_d
