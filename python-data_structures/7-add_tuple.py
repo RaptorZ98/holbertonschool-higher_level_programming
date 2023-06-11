134d2ede4db04bbe8b44e7dec3823b206ebea9f8
RaptorZ98 <federicoheuercor@hotmail.com>
2023-06-11 12:50:55 -0700

    added function 7

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
+    new_t = [0, 0]
+    for i in range(min(len(tuple_a), 2)):
+        new_t[i] += tuple_a[i]
+    for i in range(min(len(tuple_b), 2)):
+        new_t[i] += tuple_b[i]
+    return tuple(new_t[0], new_t[1])
