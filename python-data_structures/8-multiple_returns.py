c7f6e8efdc2e6927597e427c79cc8404673e8c37
RaptorZ98 <federicoheuercor@hotmail.com>
2023-06-11 11:30:39 -0700

    changed len

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    leng = len(sentence)
-    if leng > 0:
-        return tuple(leng, sentence[0])
+    if sentence:
+        return len(sentence), sentence[0]
     else:
-        return tuple(leng, None)
+        return 0, None
