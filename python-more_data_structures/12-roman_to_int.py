66dda7c9f2483a9b1cc519d8fe68058f8903e951
RaptorZ98 <federicoheuercor@hotmail.com>
2023-06-11 18:51:36 -0700

    pycode

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -9,7 +9,7 @@ def roman_to_int(roman_string):
                 }
         while i < (len(roman_string)):
             if i + 1 < len(roman_string)\
-                and roman_string[i: i + 2] in  dict:
+                    and roman_string[i: i + 2] in dict:
                 res += dict[roman_string[i: i + 2]]
                 i += 2
             else:
