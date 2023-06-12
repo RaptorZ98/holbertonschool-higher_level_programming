111531283e15168b727df129d79ae8ff1f258404
RaptorZ98 <federicoheuercor@hotmail.com>
2023-06-11 17:11:59 -0700

    added TypeError

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -7,5 +7,7 @@ def safe_print_list_integers(my_list=[], x=0):
             res += 1
         except ValueError:
             continue
+        except TypeError:
+            continue
     print("")
     return res
