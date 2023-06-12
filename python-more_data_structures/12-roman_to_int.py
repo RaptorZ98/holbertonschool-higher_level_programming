599402ed249be745ecfe66a8dcbbfa5d66efd6ed
RaptorZ98 <federicoheuercor@hotmail.com>
2023-06-11 18:48:46 -0700

    added function 12

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if isinstance(roman_string, str):
+        res = 0
+        i = 0
+        dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
+                'D': 500, 'M': 1000, 'IV': 4,
+                'IX': 9, 'XL': 40, 'XC': 90, 'CD': 400, 'CM': 900
+                }
+        while i < (len(roman_string)):
+            if i + 1 < len(roman_string)\
+                and roman_string[i: i + 2] in  dict:
+                res += dict[roman_string[i: i + 2]]
+                i += 2
+            else:
+                res += dict[roman_string[i]]
+                i += 1
+        return res
+    else:
+        return 0
