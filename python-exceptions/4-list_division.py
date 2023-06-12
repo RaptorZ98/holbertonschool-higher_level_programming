e619a24bb12226ec8d120e3af3ab0d189fa7e7e7
RaptorZ98 <federicoheuercor@hotmail.com>
2023-06-11 17:56:24 -0700

    added function 4

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    res = []
+    for i in range(list_length):
+        num = 0
+        try:
+            num = my_list_1 / my_list_2
+        except IndexError:
+            print("out of range")
+        except (ValueError, TypeError):
+            print("wrong type")
+        except ZeroDivisionError:
+            print("division by 0")
+        finally:
+            res[i] = num
