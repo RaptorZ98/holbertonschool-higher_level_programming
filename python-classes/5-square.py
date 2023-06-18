1b1875e8341be5709cecbeafb57161d0a6d7d959
RaptorZ98 <federicoheuercor@hotmail.com>
2023-06-18 11:08:37 -0700

    added 5

---
+++ python-classes/5-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+""" SQUARE SQUARE"""
+
+
+class Square:
+    """ class Square makes Square"""
+
+    def __init__(self, size=0):
+        if isinstance(size, int):
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    @property
+    def size(self):
+        """ getter def """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ Square setter """
+        if isinstance(value, int):
+            if value >= 0:
+                self.__size = value
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        """ area of square """
+        return self.__size ** 2
+
+    def my_print(self):
+        """ prints Square """
+        if self.size() == 0:
+            print("")
+        for i in range(self.size()):
+            for b in range(self.size()):
+                print("#", end="")
+            print("")
