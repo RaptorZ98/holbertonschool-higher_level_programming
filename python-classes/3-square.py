c370b0ecb89557b0fbae0d78a40d863ce7044a31
RaptorZ98 <federicoheuercor@hotmail.com>
2023-06-18 10:16:55 -0700

    pycode

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -13,7 +13,7 @@ class Square:
                 raise ValueError("size must be >= 0")
         else:
             raise TypeError("size must be an integer")
-        
+
     def area(self):
-        """ area of square"""
+        """ area of square """
         return self.__size ** 2
