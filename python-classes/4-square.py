3c6176a47ac20ca231fadea94c6d827973a2b3a1
RaptorZ98 <federicoheuercor@hotmail.com>
2023-06-18 10:51:44 -0700

    init

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -6,7 +6,13 @@ class Square:
     """ class Square makes Square"""
 
     def __init__(self, size=0):
-        self.size(self, size)
+        if isinstance(size, int):
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
 
     @property
     def size(self):
