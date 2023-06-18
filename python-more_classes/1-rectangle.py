29aae7531c32d90b8805194fd57e9d3fd275b9c1
RaptorZ98 <federicoheuercor@hotmail.com>
2023-06-18 15:34:12 -0700

    added comments

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -11,10 +11,12 @@ class Rectangle:
 
     @property
     def width(self):
+        """ width getter """
         return self.__width
 
     @width.setter
     def width(self, value):
+        """ width setter """
         if type(value) != int:
             raise TypeError("width must be an integer")
         if value < 0:
@@ -23,10 +25,12 @@ class Rectangle:
 
     @property
     def height(self):
+        """ height getter """
         return self.__height
 
     @height.setter
     def width(self, value):
+        """ height setter """
         if type(value) != int:
             raise TypeError("height must be an integer")
         if value < 0:
