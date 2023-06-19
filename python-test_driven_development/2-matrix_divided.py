3643e4f8d1d3b10ff2de20cb818187a52fd31403
RaptorZ98 <federicoheuercor@hotmail.com>
2023-06-18 19:50:53 -0700

    changed str for order

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -5,26 +5,24 @@
 def matrix_divided(matrix, div):
     """ matrix matrix """
 
+    err = "matrix must be a matrix (list of lists) of integers/floats"
+    err2 = "Each row of the matrix must have the same size"
     if type(matrix) != list:
-        raise TypeError("matrix must be a matrix\
-                        (list of lists) of integers/floats")
+        raise TypeError(err)
     for pos in range(len(matrix)):
         if type(matrix[pos]) != list:
-            raise TypeError("matrix must be a matrix\
-                            (list of lists) of integers/floats")
+            raise TypeError(err)
         if pos != len(matrix) - 1:
             if len(matrix[pos]) != len(matrix[pos + 1]):
-                raise TypeError("Each row of the matrix\
-                                must have the same size")
-        for obj in range(len(matrix[pos])):
-            if type(matrix[pos][obj]) not in [int, float]:
-                raise TypeError("matrix must be a matrix\
-                                (list of lists) of integers/floats")
+                raise TypeError(err2)
+        for i in range(len(matrix[pos])):
+            if type(matrix[pos][i]) not in [int, float]:
+                raise TypeError(err)
     if type(div) not in [int, float]:
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
     newMatrix = matrix.copy()
-    for i in range(len(matrix)):
-        newMatrix[i] = list(map(lambda x: round(x / div, 2), matrix[i]))
+    for n in range(len(matrix)):
+        newMatrix[n] = list(map(lambda x: round(x / div, 2), matrix[n]))
     return newMatrix
