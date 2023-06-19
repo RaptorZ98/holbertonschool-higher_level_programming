98d772175f47994adeb9c2a275e9572ef90e8e29
RaptorZ98 <federicoheuercor@hotmail.com>
2023-06-18 19:15:25 -0700

    added 5

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" text indent """
+
+
+def text_indentation(text):
+    if type(text) != str:
+        raise TypeError("text must be a string")
+    check = 0
+    for letter in text:
+        if check == 1 and letter == " ":
+            letter = ""
+        else:
+            ckeck = 0
+        if letter in ['.', '!', ':']:
+            print(f"{letter}")
+            check = 1
+        else:
+            print(f"{letter}", end="")
