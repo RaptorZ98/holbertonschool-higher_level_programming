43833ce295d612e91b0ac84d57eddc708e3f089a
RaptorZ98 <federicoheuercor@hotmail.com>
2023-06-25 14:07:36 -0700

    added isinstance to 3

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -4,9 +4,7 @@
 
 def is_kind_of_class(obj, a_class):
     """ is kind of class """
-    if obj is a_class:
-        return True
-    elif issubclass(obj, a_class):
+    if isinstance(obj, a_class):
         return True
     else:
         return False
