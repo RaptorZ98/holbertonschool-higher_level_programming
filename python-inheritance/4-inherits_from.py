1f0d0d4785f37a80f038ce8e354b0ece10c76660
RaptorZ98 <federicoheuercor@hotmail.com>
2023-06-25 14:16:30 -0700

    addded type() to object

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,7 +4,7 @@
 
 def inherits_from(obj, a_class):
     """ inherits from """
-    if issubclass(obj, a_class) and type(obj) != a_class:
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
     else:
         return False
