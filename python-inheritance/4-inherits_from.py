9f26977dd4a554b525afa6e1447629dea9e8c6a4
RaptorZ98 <federicoheuercor@hotmail.com>
2023-06-25 14:14:40 -0700

    changed 4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,4 +4,7 @@
 
 def inherits_from(obj, a_class):
     """ inherits from """
-    return issubclass(obj, a_class)
+    if issubclass(obj, a_class) and type(obj) != a_class:
+        return True
+    else:
+        return False
