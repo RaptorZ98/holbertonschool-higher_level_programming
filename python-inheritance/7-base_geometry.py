af6c84e81e5fe9fb2c64ac49247a9b08a7245569
RaptorZ98 <federicoheuercor@hotmail.com>
2023-06-25 14:25:55 -0700

    added 7

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" base geo """
+
+
+class BaseGeometry:
+    """ base geo """
+    def area(self):
+        """ area of """
+        raise Exception("area() is not implemented")
+    def integer_validator(self, name, value):
+        """ validates value """
+        if type(value) != int:
+            raise TypeError(name + " must be an integer")
+        elif value < 0:
+            raise ValueError(name + " must be greater than 0")
