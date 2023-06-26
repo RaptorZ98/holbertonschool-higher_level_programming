9459f966f5979e6b0a47de6204b1e63bb662f9b5
RaptorZ98 <federicoheuercor@hotmail.com>
2023-06-25 18:02:15 -0700

    added 7

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" add item """
+
+
+import sys
+sjf = __import__('5-save_to_json_file').save_to_json_file
+ljf = __import__('6-load_from_json_file').load_from_json_file
+
+
+try:
+    list = ljf("add_item.json")
+except FileNotFoundError:
+    list = []
+
+for argument in sys.argv:
+    list.append(argument)
+
+sjf(list, "add_item.json")
