7bc2aea31c9340df4c1b7ce741fe991ec7a60cb3
RaptorZ98 <federicoheuercor@hotmail.com>
2023-06-25 18:45:28 -0700

    added 10

---
+++ python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" student student """
+
+
+class Student:
+    """ student student """
+    def __init__(self, first_name, last_name, age):
+        """ init init """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if type(attrs) == list:
+            new_dic = {}
+            i = 0
+            for att in self.__dict__:
+                if att in attrs:
+                    new_dic[i] = att
+                    i += 1
+            return new_dic
+        else:
+            return self.__dict__
