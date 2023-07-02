a368709118c28f7499f3cbafa19cbcbe45cb1c4f
RaptorZ98 <federicoheuercor@hotmail.com>
2023-07-01 21:21:23 -0700

    added rectangle with 2 and 3

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python3
+""" class rectangle """
+from base import Base
+
+
+class Rectangle(Base):
+    """ class rectangle """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """ class init """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """ getter width """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ width setter """
+        if isinstance(value, int):
+            if value > 0:
+                self.__width = value
+            else:
+                raise ValueError("width must be > 0")
+        else:
+            raise TypeError("width must be an integer")
+
+    @property
+    def height(self):
+        """ getter height """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ height setter """
+        if isinstance(value, int):
+            if value > 0:
+                self.__height = value
+            else:
+                raise ValueError("height must be > 0")
+        else:
+            raise TypeError("height must be an integer")
+
+    @property
+    def x(self):
+        """ getter x """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """ x setter """
+        if isinstance(value, int):
+            if value >= 0:
+                self.__x = value
+            else:
+                raise ValueError("x must be >= 0")
+        else:
+            raise TypeError("x must be an integer")
+
+    @property
+    def y(self):
+        """ getter y """
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """ y setter """
+        if isinstance(value, int):
+            if value >= 0:
+                self.__y = value
+            else:
+                raise ValueError("y must be >= 0")
+        else:
+            raise TypeError("y must be an integer")
