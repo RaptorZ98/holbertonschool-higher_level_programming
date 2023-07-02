985bbd5795182181dae301a01893a08f4c735620
RaptorZ98 <federicoheuercor@hotmail.com>
2023-07-02 13:50:53 -0700

    added 15 and 16

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ class base """
+import json
 
 
 class Base:
@@ -13,3 +14,25 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """ dict to json """
+        if list_dictionaries is None and len(list_dictionaries) != 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ json string to file """
+        name = f"{cls.__name__}.json"
+
+        with open(name, 'w') as f:
+            if list_objs is None:
+                list = []
+            else:
+                list = []
+                for obj in list_objs:
+                    list.append(obj.to_dictionary())
+            f.write(cls.to_json_string(list))
