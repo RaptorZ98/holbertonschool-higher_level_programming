41902d478caeefa2d6567e095622cff2beb2055c
RaptorZ98 <federicoheuercor@hotmail.com>
2023-07-01 20:51:54 -0700

    added the start of project almost a circle

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" class base """
+
+
+class Base:
+    """ class base """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ init of class base """
+        if id == None:
+            self.id = id
+        else:
+            self.__nb_objects =+ 1
+            self.id = self.__nb_objects
