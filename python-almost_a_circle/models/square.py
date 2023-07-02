f42b70829e6ae24ef357f7a3b664df926deadfa7
RaptorZ98 <federicoheuercor@hotmail.com>
2023-07-02 12:23:20 -0700

    added new __str__ method

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -8,3 +8,8 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """ init for class square """
         super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """ str of the square """
+        return f"[Square] ({self.id}) {self.__x}/{self.__y} \
+- {self.__width}/{self.__height}"
