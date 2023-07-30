b6fef65f187bd4420a893bde7496e29328cb26f5
RaptorZ98 <federicoheuercor@hotmail.com>
2023-07-30 15:07:37 -0700

    changed id

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -9,7 +9,7 @@ if __name__ == "main":
     db = MySQLdb.connect(host='localhost', user=argv[1],
                          passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT id, name FROM states ORDER BY states.id ASC")
+    cur.execute("SELECT id, name FROM states ORDER BY id ASC")
     rows = cur.fetchall()
     for row in rows:
         for col in row:
