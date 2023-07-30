dc2b501ba7d3efd38de17d2330a61ab5ca630812
RaptorZ98 <federicoheuercor@hotmail.com>
2023-07-30 16:35:58 -0700

    m

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute(f"SELECT cities.name\
+    cur.execute("SELECT cities.name\
  FROM cities JOIN states ON \
 states.id = cities.state_id WHERE states.name = '{argv4}' \
 ORDER BY id ASC;")
