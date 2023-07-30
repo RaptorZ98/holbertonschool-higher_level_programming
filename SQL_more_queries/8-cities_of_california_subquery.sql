e31ae9d86810be7a096be017f3868c0dc64fe904
RaptorZ98 <federicoheuercor@hotmail.com>
2023-07-30 08:36:06 -0700

    mayus

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- cities of california subquery
-SELECT id, name FROM cities WHERE state_id = (SELECT id From states WHERE name = 'California') ORDER BY ASC;
+SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California') ORDER BY ASC;
