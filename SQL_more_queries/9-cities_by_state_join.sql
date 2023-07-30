40ab9fbed5a39f7c7877c43ef534f000363fefbe
RaptorZ98 <federicoheuercor@hotmail.com>
2023-07-30 08:50:15 -0700

    changed state for states

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- cities by state join
-SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.state_id = state.id ORDER BY cities.id ASC;
+SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC;
