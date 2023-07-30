a755c5c6bffd5f46415056d20d97b70e3b73a8f2
RaptorZ98 <federicoheuercor@hotmail.com>
2023-07-30 15:51:02 -0700

    added binary

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
                          passwd=argv[2], db=argv[3])
     cur = db.cursor()
     cur.execute("SELECT id, name FROM states WHERE name = \
-'{}' ORDER BY id ASC".format(argv[4]))
+BINARY '{}' ORDER BY id ASC".format(argv[4]))
     rows = cur.fetchall()
     for row in rows:
         print(row)
