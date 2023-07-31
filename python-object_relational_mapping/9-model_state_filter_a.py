77fbf8b0feee75350d9b36e49fa3554947d36049
RaptorZ98 <federicoheuercor@hotmail.com>
2023-07-30 19:25:23 -0700

    added 9

---
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" select states """
+
+
+import MySQLdb
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from sqlalchemy import select
+
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@local\
+host:3306/{}".format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    with Session(engine) as session:
+        query = select(State).where(State.name.like('%a%')).order_by(State.id)
+        result = session.execute(query).first()
+    for row in result:
+        print(f"{row[0].id}: {row[0].name}")
